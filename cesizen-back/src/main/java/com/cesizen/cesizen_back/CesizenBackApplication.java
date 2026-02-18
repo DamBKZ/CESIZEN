a4ce024a7d6f331aa1420221528c42fc67e7e150
Damien <baczkiewicz.damien@hotmail.fr>
2026-02-18 21:22:27 +0100

    first commit

---
+++ cesizen-back/src/main/java/com/cesizen/cesizen_back/CesizenBackApplication.java
@@ -0,0 +1,13 @@
+package com.cesizen.cesizen_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CesizenBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CesizenBackApplication.class, args);
+	}
+
+}
---
+++ cesizen-back/src/test/java/com/cesizen/cesizen_back/CesizenBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cesizen.cesizen_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CesizenBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
