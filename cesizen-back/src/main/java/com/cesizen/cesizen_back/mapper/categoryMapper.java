04f3c6728e4dced0ddb031bedd26d019f1b65015
Damien <baczkiewicz.damien@hotmail.fr>
2026-05-31 08:55:10 +0200

    fix:CategoryMapperImpl

--- cesizen-back/src/main/java/com/cesizen/cesizen_back/mapper/categoryMapper.java
+++ cesizen-back/src/main/java/com/cesizen/cesizen_back/mapper/categoryMapper.java
@@ -4,14 +4,19 @@ import com.cesizen.cesizen_back.dto.user.CategoryRequest;
 import com.cesizen.cesizen_back.dto.user.CategoryResponse;
 import com.cesizen.cesizen_back.entity.Category;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring")
 public interface CategoryMapper {
 
+    @Mapping(target = "categoryId", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
     Category toEntity(CategoryRequest request);
 
     CategoryResponse toResponse(Category category);
 
+    @Mapping(target = "categoryId", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
     void updateEntity(@MappingTarget Category category, CategoryRequest request);
 }
