6448723fa127fb4d053df2c1f9454b48503d60da
Damien <baczkiewicz.damien@hotmail.fr>
2026-07-22 23:53:02 +0200

    chore(front): remove unused legacy modules

--- cesizen-front/src/app/logs/list/list.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-logs-list',
-  standalone: true,
-  templateUrl: './list.component.html',
-  styleUrls: ['./list.component.scss']
-})
-export class ListComponent {}
